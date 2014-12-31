8078e2e2593ed0189a6ff4717c9721da2d956aaa
niketpathak <niketpathak.gkm@gmail.com>
2014-12-31 02:07:02 +0100

    launch_process update

--- src/test/launch_process.java
+++ src/test/launch_process.java
@@ -6,25 +6,22 @@ public class launch_process {
 	public static void main(String args[]) {
 		 
         try {
-//            Runtime rt = Runtime.getRuntime();
-            //Process pr = rt.exec("cmd /c dir");
-        	
-//            File file = new File("/Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test");         
-//        	File file = new File("/usr/local/bin");     // this is actually the location of Tesseract Prog    
-//        	, null, file   
+
         	Runtime runtime = Runtime.getRuntime();
-            Process pr = runtime.exec("tesseract /Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test/recomm1.jpg /Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test/niket1 -l eng");
-//        	Process pr = runtime.exec("identify /Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test/recomm1.jpg");
+//          Process pr = runtime.exec("tesseract /Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test/recomm1.jpg /Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test/niket1 -l eng");
+        	Process pr = runtime.exec("identify /Users/niketpathak/Documents/ISEP_sem2/IMG_processing/Research/img2text_test/recomm1.jpg");
 
             BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
             BufferedReader error_if_any = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
             String line=null;
             String err =null;
             while((line=input.readLine()) != null) {
-                System.out.println(line);
+                System.out.println("This is the output->"+line);
+                String[] expected_ext = {"JPEG","PNG","TIFF"};
+                
             }
             while ((err = error_if_any.readLine()) != null) {
-                System.out.println(err);
+                System.out.println("This is Err Code->"+err);
             }
             int exitVal = pr.waitFor();
             System.out.println("Exited with error code "+exitVal);
